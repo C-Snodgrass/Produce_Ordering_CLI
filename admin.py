a18c9a6a39c297fb60f18806d7c2d1647bed0ea4
Chris Snod <deans3@utas.edu.au>
2024-04-25 10:57:56 +1000

    Inital commit... functions with no body are present

---
+++ admin.py
@@ -0,0 +1,37 @@
+def admin_login():
+    # promt user to enter admin user and password or return to main menu...
+    # read username and ps form db
+    return
+
+
+def display_good():
+    # display record of what is in the DB i.e. products and prices
+    return
+
+
+def update_product():
+    # validate and add... valid?? return to admin menu
+    return
+
+
+def add_product():
+    # validate and add... valid?? return to admin menu
+    return
+
+
+def admin_menu():
+    while True:
+        print("╔══════════════════════════════════════════╗")
+        print("║         * Admin Options *                ║")
+        print("║   Welcome, administrator!                ║")
+        print("║   Please choose from the following       ║")
+        print("║   options:                               ║")
+        print("║                                          ║")
+        print("║   0. Quit (End session)                  ║")
+        print("║   2. Return to Main Menu                 ║")
+        print("║   3. Add a New Product                   ║")
+        print("║   4. Update Product Information          ║")
+        print("╚══════════════════════════════════════════╝")
+# TODO if else statments for the menu... validate input
+
+
---
+++ main.py
@@ -0,0 +1,56 @@
+# Chris Snodgrass 113255
+
+def printWelcome():
+    print("***********************************************")
+    print("*          Welcome to the Stardew Valley       *")
+    print("*            Online Store & Emporium          *")
+    print("*                                             *")
+    print("*          Explore Our Bountiful Selection     *")
+    print("*            of Seeds, Tools, and More!        *")
+    print("*                                             *")
+    print("*        Step Into a World of Farming Magic    *")
+    print("*                                             *")
+    print("*        Please select from the following      *")
+    print("*                 options below:              *")
+    print("*                                             *")
+    print("*      1. Buy Products                         *")
+    print("*      2. Admin Login                          *")
+    print("*      0. Quit                                 *")
+    print("***********************************************")
+
+
+def main_menu():
+    while True:
+        usr_selection = input()
+        if (usr_selection == 0):
+            print("Good Bye")
+            break
+        elif (usr_selection == 1):
+            print("selection")
+        elif (usr_selection == 2):
+            print("selection")
+        else:
+            print("Invalid selection...")
+
+
+# caclulate the order value
+def calc_order_cost():
+    return
+
+
+# write the order to disk
+def write_order():
+    return
+
+
+def buy_products():
+    '''
+    TODO Read Products and prices form DB
+    Display list of produce and prces on the screen with instrucitons for customer order produce
+    validate input
+    calc price and display order details
+    ask for confirmaiton
+    yes -> write to disk && aks to quit or return to menu
+    no ->  ask to quit or return to menu
+    '''
+
